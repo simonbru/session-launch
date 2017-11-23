9fe63c0f39c2c8008accca5c91f0b7d6432c1ff3
Simon Brulhart <simon@brulhart.me>
2017-11-23 17:11:07 +0100

    Add rust client

---
+++ rust/src/bin/rust-client.rs
@@ -0,0 +1,49 @@
+extern crate dbus;
+
+use dbus::{Connection, BusType, Message};
+use dbus::arg::Array;
+use std::env;
+use std::process::exit;
+
+fn main() {
+    let con = Connection::get_private(BusType::Session).unwrap();
+
+    let current_dir = env::current_dir().unwrap();
+//    let mut sysargs= env::args();
+//    let procname = sysargs.next().unwrap();
+//    let executable = sysargs.next().unwrap();
+//    let args: Vec<String> = sysargs.collect();
+
+    let sysargs: Vec<String> = env::args().collect();
+    if sysargs.len() < 2 {
+        eprintln!("ERROR: Not enough arguments");
+        exit(111);
+    }
+//    let [procname, executable, ...] = sysargs;
+    let procname = &sysargs[0];
+    let executable = &sysargs[1];
+    let args = &sysargs[2..];
+
+    let synchronous_exec = procname.to_lowercase().contains("exec");
+
+    let method_name = if synchronous_exec { "Exec" } else { "Open" };
+    let msg = Message::new_method_call(
+        "simonbru.SessionLaunch",
+        "/simonbru/SessionLaunch",
+        "simonbru.SessionLaunch",
+        method_name
+        )
+        .unwrap()
+        .append3(current_dir.to_str().unwrap(), executable, args);
+
+    let resp = con.send_with_reply_and_block(msg, -1);
+    let exit_code = match resp {
+        Ok(ref msg) if synchronous_exec => msg.get1::<i32>().unwrap(),
+        Ok(_) => 0,
+        Err(error) => {
+            println!("{:?}", error);
+            222
+        }
+    };
+    exit(exit_code);
+}
